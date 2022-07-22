2c09e0fbb7303fa3aed5cc4a599032e0a37583b0
Daphne <daphneruth304@gmail.com>
2022-07-22 10:00:26 +0300

    updated css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import {Route} from 'react-router-dom';
 import Welcome from './pages/Welcome';
 
 import Products from './pages/Products';
+import MainHeader from './Components/MainHeader';
 function App() {
   return (
     <div>
+      <MainHeader />
      <Route path="/Welcome">
 
     <Welcome/>
