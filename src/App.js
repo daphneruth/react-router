bd4f597baeb9da72af90a4bdf6cf9f808ee0d888
Daphne <daphneruth304@gmail.com>
2022-07-22 09:14:50 +0300

    route path

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import {Route} from 'react-router-dom';
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+     <Route path="/Welcome">
+
+      
+     </Route>
     </div>
   );
 }
