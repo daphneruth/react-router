5762f3b8555a2cc849d0584ffc5ef126a9c7525f
Daphne <daphneruth304@gmail.com>
2022-07-22 10:10:00 +0300

    using NavLink to replace anchor tags

--- src/Components/MainHeader.js
+++ src/Components/MainHeader.js
@@ -1,19 +1,21 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import classes from './MainHeader.css';
 
 const MainHeader = () =>
 {
-
+<header className={classes.header}>
 <nav>
     <ul>
         <li>
-            <Link to ="/Welcome">Welcome</Link>
+            <NavLink activeClassName="={classes.active" to ="/Welcome">Welcome</NavLink>
         </li>
 
         <li>
-        <Link to ="/Products">Products</Link> 
+        <NavLink activeClassName="={classes.active" to ="/Products">Products</NavLink> 
         </li>
     
     </ul>
 </nav>
+</header>
 }
 export default MainHeader;
