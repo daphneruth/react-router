0c66a97c4e7577754d0160f1e82cf0a6dfdc1726
Daphne <daphneruth304@gmail.com>
2022-07-22 09:33:44 +0300

    updated links

--- src/Components/MainHeader.js
+++ src/Components/MainHeader.js
@@ -2,5 +2,17 @@
 const MainHeader = () =>
 {
 
+<nav>
+    <ul>
+        <li>
+            <a href="/Welcome">Welcome</a>
+        </li>
+
+        <li>
+            <a href="/Products">Products</a>
+        </li>
+    
+    </ul>
+</nav>
 }
 export default MainHeader;
