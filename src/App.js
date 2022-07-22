19334baf7931e690cc5684b7eeb9c739271a2719
Daphne <daphneruth304@gmail.com>
2022-07-22 09:17:00 +0300

    rendering components using route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import {Route} from 'react-router-dom';
+import Welcome from './components/Welcome';
 
+import Products from './components/Products';
 function App() {
   return (
     <div>
      <Route path="/Welcome">
 
-      
+    <Welcome/>
      </Route>
+     <Route path="/Products">
+
+      <Products/>
+      </Route>
     </div>
   );
 }
